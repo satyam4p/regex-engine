e6e21b8134c8e6089eb9c2f5658c74eebbbe0702
satyam4p <satyamkumar343@gmail.com>
2024-06-23 18:17:35 +0530

    add closure function

--- operationsOnNFA.js
+++ operationsOnNFA.js
@@ -17,4 +17,23 @@ function uniion(firstState, secondState) {
 }
 
 /**does concatenation operation between two states */
-function concat(firstState, secondState) {}
+function concat(firstState, secondState) {
+  transitions.addEpsilonTransition(firstState.end, secondState.start);
+  firstState.end.isEnd = false;
+  return { start: firstState.start, end: secondState.end };
+}
+
+/**kleen star closure */
+function closure(nfa) {
+  const start = createState(false);
+  const end = createState(true);
+
+  addEpsilonTransition(start, end);
+  addEpsilonTransition(start, nfa.start);
+
+  addEpsilonTransition(nfa.end, end);
+  addEpsilonTransition(nfa.end, nfa.start);
+  nfa.end.isEnd = false;
+
+  return { start, end };
+}
